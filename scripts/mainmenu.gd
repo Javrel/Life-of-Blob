4cc289b121b66d46445daaee96cc28602275bff2
Javrel <javrel@btinternet.com>
2023-07-09 22:20:07 +0100

    refactor main menu code to allow for future transition add hover and select sounds to meny

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -3,18 +3,59 @@ extends Node2D
 
 export var mainGameScene : PackedScene
 
+var menu_fade_out = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body. 
+	pass
 
+func play_hover():
+	$Audio/Hover.play()
+
+func play_select():
+	$Audio/Select.play()
+	
+func transition(scene):
+	if scene:
+		get_tree().change_scene(scene)
+	else:
+		get_tree().quit()
+		
+		
 func _on_QuitButton_button_up():
-	get_tree().quit()
-
-
+	transition("")
+	
 func _on_NewGame2_button_up():
-	get_tree().change_scene("res://level2.tscn")
-
-
+	transition("res://level2.tscn")
+	
 func _on_NewGame1_button_up():
-	get_tree().change_scene("res://level1.tscn")
+	transition("res://level1.tscn")
+	
+func _on_OptionsButton_button_up():
+	pass # Replace with function body.
+	
+func _on_NewGame1_mouse_entered():
+	play_hover()
+	
+func _on_NewGame2_mouse_entered():
+	play_hover()
+
+func _on_OptionsButton_mouse_entered():
+	play_hover()
+
+func _on_QuitButton_mouse_entered():
+	play_hover()
+
+func _on_QuitButton_button_down():
+	play_select()
+
+func _on_OptionsButton_button_down():
+	play_select()
+
+func _on_NewGame2_button_down():
+	play_select()
+
+func _on_NewGame1_button_down():
+	play_select()
+
+
