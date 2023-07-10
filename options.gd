5dce9cb9895958b398e73a4d77f96393214ec64c
Javrel <javrel@btinternet.com>
2023-07-11 00:20:23 +0100

    get volume level from master bus when entering options

--- options.gd
+++ options.gd
@@ -14,3 +14,7 @@ func _on_VolumeSlider_value_changed(value):
 func _on_back_button_down():
 	play_select()
 	Transition.change_scene("res://mainmenu.tscn") # Replace with function body.
+
+
+func _on_VolumeSlider_ready():
+	$MenuContainer/VBoxContainer/VBoxContainer/MarginContainer/VolumeSlider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
